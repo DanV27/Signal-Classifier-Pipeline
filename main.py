955bd110b1982c0013c106e81e934755b13eaa98
Dani Valenzuela <vdanny911@gmail.com>
2026-09-18 12:47:12 -0500

    NEW FUNCTIONS/ DATA: data: loaded 200 rows from dataset:"MLCommons/peoples_speech" on to local storage. function run(): loads data and saves on to disk function load(): as for now loads it and prints its info

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from datasets import Dataset, load_dataset, Audio, load_from_disk
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def run():
+    ds = load_dataset("MLCommons/peoples_speech", "clean", split="train", streaming=True)
+    ds = ds.cast_column("audio", Audio(decode=False))
+    small = ds.take(200)
+
+    small_ds = Dataset.from_list(list(small))
+    small_ds.save_to_disk("data/peoples_speech_sample")
+
+def load():
+    ds = load_from_disk("data/peoples_speech_sample")
+
+    print(type(ds))
+    print(ds)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    #run()
+
+    load()
